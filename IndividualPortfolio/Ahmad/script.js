9b6a19d5333ada7a261d25077d7ca049f7fde65c
Slimezzz-s1 <dixxalet581@gmail.com>
2026-05-24 21:06:42 +0700

    Initializes my profile

--- IndividualPortfolio/Ahmad/script.js
+++ IndividualPortfolio/Ahmad/script.js
@@ -1,1 +1,13 @@
 "use strict";
+//  ROOT
+const navHeaderButtons = document.querySelectorAll('.nav-header-link');
+navHeaderButtons.forEach((navHeaderButton) => {
+    navHeaderButton.addEventListener('click', () => {
+        document.querySelector(`#${navHeaderButton.id.toString().replace('nav-header-link-', '')}`)?.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+//  HOME
+//  ABOUT
+//  SKILLS
+//  PORTFOLIO
+//  CONTACT
--- IndividualPortfolio/Ahmad/script.ts
+++ IndividualPortfolio/Ahmad/script.ts
@@ -1,4 +1,15 @@
 //  ROOT
+
+const navHeaderButtons : NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.nav-header-link')
+
+navHeaderButtons.forEach((navHeaderButton) =>
+{
+    navHeaderButton.addEventListener('click', () =>
+    {
+        document.querySelector(`#${navHeaderButton.id.toString().replace('nav-header-link-', '')}`)?.scrollIntoView({behavior : 'smooth'})
+    })
+})
+
 //  HOME
 //  ABOUT
 //  SKILLS
